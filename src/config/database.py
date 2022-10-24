0c4657227602adeeb4ff70ef26cf6eb5beebea66
Jaume Moreno <morenocantoj@gmail.com>
2022-10-24 14:43:05 +0200

    chore: minimun database support

---
+++ src/config/database.py
@@ -0,0 +1,31 @@
+"""
+    Database configuration
+"""
+import os
+
+
+DATABASE_CONFIG = {
+    "host": os.environ.get("DATABASE_HOST", "localhost"),
+    "port": int(
+        os.environ.get("DATABASE_PORT", "5432"),
+    ),
+    "username": os.environ.get("DATABASE_USER", "oipie"),
+    "password": os.environ.get("DATABASE_PASSWORD", "password"),
+    "database_name": os.environ.get("DATABASE_NAME", "oipie"),
+}
+
+
+def database_url_connection(override_config: dict or None = None):
+    """
+    Builds PostgreSQL database connection
+    """
+    database_config = {**DATABASE_CONFIG, **(override_config or {})}
+
+    return os.environ.get(
+        "DATABASE_URL",
+        (
+            f"postgresql://{database_config['host']}:{database_config['port']}"
+            f"/{database_config['database_name']}"
+            f"?user={database_config['username']}&password={database_config['password']}"
+        ).replace("postgres://", "postgresql://"),
+    )
--- src/main.py
+++ src/main.py
@@ -2,14 +2,40 @@
 App definition
 """
 
+import sqlalchemy
+import databases
 from fastapi import FastAPI
+from src.config.database import database_url_connection
+
+DATABASE_URL = database_url_connection()
+
+database = databases.Database(DATABASE_URL)
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
 app = FastAPI()
 
-@app.get('/health')
+
+@app.on_event("startup")
+async def startup():
+    """
+    Method executed when API is started
+    """
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    """
+    Method executed when API is shutdown
+    """
+    await database.disconnect()
+
+
+@app.get("/health")
 async def health():
     """
     Returns 200 OK if service is alive
     """
     return {"status": "OK"}
-    
