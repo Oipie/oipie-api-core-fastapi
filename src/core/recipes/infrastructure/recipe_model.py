34b0fc697d4195d8903e3512358b80f6456f20b1
Jaume Moreno <morenocantoj@gmail.com>
2022-10-26 14:32:23 +0200

    feat: recipe sqlalchemy model and migration

---
+++ src/core/recipes/infrastructure/recipe_model.py
@@ -0,0 +1,51 @@
+"""
+Recipe Database Model
+"""
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, String, Integer
+from src.dependencies.database import BaseModel
+from src.core.recipes.domain.recipe import Recipe
+
+
+class RecipeModel(BaseModel):
+    """
+    This class represents a Recipe in database model
+    """
+
+    __tablename__ = "recipes"
+
+    uuid = Column(
+        UUID(as_uuid=True), default=uuid.uuid4, nullable=False, primary_key=True
+    )
+    name = Column(String, nullable=False)
+    favourite_amount = Column(Integer, default=0, nullable=False)
+    preparation_time = Column(Integer, default=0, nullable=False)
+    cover = Column(String)
+
+    @staticmethod
+    def from_domain_object(recipe: Recipe):
+        """
+        Transforms to Recipe model
+        """
+        return RecipeModel(
+            uuid=recipe.uuid,
+            name=recipe.uuid,
+            favourite_amount=recipe.favourite_amount,
+            preparation_time=recipe.preparation_time,
+            cover=recipe.preparation_time,
+        )
+
+    def to_domain_object(self) -> Recipe:
+        """
+        Transforms Recipe database model to a domain object
+        """
+        return Recipe(
+            {
+                "uuid": self.uuid,
+                "name": self.name,
+                "favourite_amount": self.favourite_amount,
+                "preparation_time": self.preparation_time,
+                "cover": self.cover,
+            }
+        )
