d0239d8fb43ee4a02ff3127da02fd852a70972f9
Jaume Moreno <morenocantoj@gmail.com>
2022-10-26 10:32:08 +0200

    feat: make database session a dependency

---
+++ src/dependencies/database.py
@@ -0,0 +1,22 @@
+"""
+Database dependencies
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.declarative import declarative_base
+from src.config.database import database_url_connection
+
+engine = create_engine(database_url_connection())
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+
+def get_database() -> Session:
+    """
+    Returns a Session instance
+    """
+    database = SessionLocal()
+    try:
+        yield database
+    finally:
+        database.close()
