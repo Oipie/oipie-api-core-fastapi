4acf296cec1791adce130118f9f1fe9fe6ba9576
Jaume Moreno <morenocantoj@gmail.com>
2022-11-07 12:36:54 +0100

    feat: hash password when registering an user

--- src/tests/v1/users/test_create_users.py
+++ src/tests/v1/users/test_create_users.py
@@ -3,7 +3,6 @@ E2E tests for user creation
 """
 
 from http import HTTPStatus
-import pytest
 from src.api.routers.users.models.user_create_dto import UserCreateDto
 from src.tests.fixtures.user_fixture import JANE, JOHN
 from src.tests.utils.api_client import ApiClient
